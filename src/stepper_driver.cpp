bcd5f5780601a4999c094107b65aac98566a3110
Brandon Brown <brandonbrown@openmyr.com>
2018-11-11 17:58:23 -0500

    Stepper driver uses new pulse counter library Fixes bug where slow speeds end far too soon

--- src/stepper_driver.cpp
+++ src/stepper_driver.cpp
@@ -1,7 +1,7 @@
 #include "include/stepper_driver.h"
+#include "include/waveformPulseCounter.h"
 #include "Arduino.h"
 #include "include/task_prio.h"
-#include "core_esp8266_waveform.h"
 
 #define GPIO_STEP 4
 #define GPIO_STEP_ENABLE 5
@@ -14,43 +14,39 @@
 
 uint32_t direction = 0;
 uint32_t paused = 0;
-uint32_t limit = 0;
 int32_t location = 0;
 uint32_t command_done = 1;
 
 extern "C"
 {
+  ICACHE_RAM_ATTR void ppcb()
+  {
+      if(paused == 0)
+      {
+        if(direction == 1)
+          location++;
+        else
+          location--;
+      }
+  }
 
-    uint32_t ICACHE_RAM_ATTR stepper_isr()
+  ICACHE_RAM_ATTR void pdcb()
+  {
+    command_done = 1;
+    if(paused == 1)
     {
-        limit--;
-        if(paused == 0)
-        {
-            if(direction == 1)
-                location++;
-            else
-                location--;
-        }
-
-        if(limit == 0)
-        {
-            stopWaveform(GPIO_STEP);
-            command_done = 1;
-            if(paused == 1)
-            {
-                paused = 0;
-                digitalWrite(GPIO_STEP_ENABLE, 0);
-            }
-            system_os_post(ACK_TASK_PRIO, 0, 0);
-        }
-        return UINT32_MAX;
+      paused = 0;
+      digitalWrite(GPIO_STEP_ENABLE, 0);
     }
+    system_os_post(ACK_TASK_PRIO, 0, 0);
+  }
 }
 
 stepper_driver::stepper_driver() : motor_driver()
 {
-    setTimer1Callback(&stepper_isr);
     init_motor_gpio();
+    setPerPulseCallback(ppcb);
+    setPulsesDepletedCallback(pdcb);
 }
 
 void stepper_driver::init_motor_gpio()
@@ -61,6 +57,8 @@ void stepper_driver::init_motor_gpio()
     pinMode(GPIO_STEP_DIR, OUTPUT);
     pinMode(GPIO_USTEP_A, OUTPUT);
 
+    setWaveformPulseCountPin(GPIO_STEP);
+
     digitalWrite(GPIO_USTEP_A, 0);
     digitalWrite(GPIO_STEP_ENABLE, 0);
 }
@@ -68,14 +66,15 @@ void stepper_driver::init_motor_gpio()
 void stepper_driver::opcode_move(signed int step_num, unsigned short step_rate, uint8_t motor_id)
 {
     //Serial.printf("move %d steps at %d steps per sec", step_num, step_rate);
-    stopWaveform(GPIO_STEP);
-    limit = abs(step_num) * 2;
+    stopWaveform();
+    
+    uint32_t limit = abs(step_num);
     direction = step_num > 0 ? 1 : 0;
     digitalWrite(GPIO_STEP_DIR, direction);
     if(limit > 0)
     {
         command_done = 0;
-        startWaveform(GPIO_STEP, 10, (1 / ((float)step_rate) * 1000000) - 10, 0);
+        startWaveform(10, (1 / ((float)step_rate) * 1000000) - 10, limit);
     }
     else
     {
@@ -86,23 +85,26 @@ void stepper_driver::opcode_move(signed int step_num, unsigned short step_rate, 
 
 void stepper_driver::opcode_goto(signed int step_num, unsigned short step_rate, uint8_t motor_id)
 {
-    stopWaveform(GPIO_STEP);
+    stopWaveform();
     //Serial.printf("goto %d %d\n", step_num, step_rate);
+    
+    uint32_t limit = 0;
+
     if(location > step_num)
     {
-        limit = (location - step_num) * 2;
+        limit = (location - step_num);
         direction = 0;
     }
     else if(location < step_num)
     {
-        limit = (step_num - location) * 2;
+        limit = (step_num - location);
         direction = 1;
     }
     digitalWrite(GPIO_STEP_DIR, direction);
     if(limit > 0)
     {
         command_done = 0;
-        startWaveform(GPIO_STEP, 10, (1 / ((float)step_rate) * 1000000) - 10, 0);
+        startWaveform(10, (1 / ((float)step_rate) * 1000000) - 10, limit);
     }
     else
     {
@@ -113,9 +115,9 @@ void stepper_driver::opcode_goto(signed int step_num, unsigned short step_rate, 
 
 void stepper_driver::opcode_stop(signed int wait_time, unsigned short precision, uint8_t motor_id)
 {
-    stopWaveform(GPIO_STEP);
+    stopWaveform();
     //Serial.printf("stop %d %d\n", wait_time, precision);
-    limit = abs(wait_time) * 2;
+    uint32_t limit = abs(wait_time);
     if(limit == 0)
     {
         command_done = 1;
@@ -126,7 +128,7 @@ void stepper_driver::opcode_stop(signed int wait_time, unsigned short precision,
         command_done = 0;
         paused = 1;
         digitalWrite(GPIO_STEP_ENABLE, 1);
-        startWaveform(GPIO_STEP, 10, (1 / ((float)precision) * 1000000) - 10, 0);
+        startWaveform(10, (1 / ((float)precision) * 1000000) - 10, limit);
     }
 }
 
